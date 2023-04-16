6e26301650f8679891674f68a396bb6626dccf55
Mark Mennell <mark.mennell@fmgl.com.au>
2023-04-16 11:33:04 +0800

    adding table defs

--- src/fmsgid.go
+++ src/fmsgid.go
@@ -5,9 +5,53 @@ import (
 	"fmt"
 
 	"github.com/gin-gonic/gin"
+	"github.com/jackc/pgx/v4"
 	"github.com/jackc/pgx/v4/pgxpool"
 )
 
+var sqlTables = `
+create extension if not exists citext;
+
+create table if not exists address (
+    address 					citext 	primary key,
+    display_name 				text,
+    accepting_new 				bool	not null default true,
+    limit_recv_size_total 		bigint	not null default -1,
+    limit_recv_size_per_msg 	bigint	not null default -1,
+    limit_recv_size_per_1d 		bigint	not null default -1,
+    limit_recv_count_per_1d 	bigint	not null default -1,
+    limit_send_size_total 		bigint	not null default -1,
+    limit_send_size_per_msg 	bigint	not null default -1,
+    limit_send_size_per_1d 		bigint	not null default -1,
+    limit_send_count_per_1d 	bigint	not null default -1
+);
+
+-- TODO consider time-series datastore e.g. TimescaleDB
+create table if not exists address_tx (
+	address	citext		not null references address (address),
+	ts		timestamp	not null,
+	op		varchar(5)	not null, -- send, recv
+	size	int			not null,
+	primary key (address, ts)
+)
+`
+
+func initDb() error {
+	db, err := pgx.Connect(context.Background(), "")
+	if err != nil {
+		return err
+	}
+	defer db.Close()
+	err = db.Ping()
+	if err != nil {
+		return err
+	}
+	_, err = db.Exec(sqlTables)
+	if err != nil {
+		return err
+	}
+	return nil
+}
 
 func getAddressDetail(c *gin.Context) {
 	ctx := context.Background()
@@ -35,14 +79,14 @@ func getAddressDetail(c *gin.Context) {
 		var address string
 		var displayName string
 		var acceptingNew bool
-		var limitRecvSizeTotal int
-		var limitRecvSizePerMsg int
-		var limitRecvSizePer1d int
-		var limitRecvCountPer1d int
-		var limitSendSizeTotal int
-		var limitSendSizePerMsg int
-		var limitSendSizePer1d int
-		var limitSendCountPer1d int
+		var limitRecvSizeTotal int64
+		var limitRecvSizePerMsg int64
+		var limitRecvSizePer1d int64
+		var limitRecvCountPer1d int64
+		var limitSendSizeTotal int64
+		var limitSendSizePerMsg int64
+		var limitSendSizePer1d int64
+		var limitSendCountPer1d int64
 		var recvSizeTotal int64
 		var recvSizePer1d int64
 		var recvCountPer1d int64
